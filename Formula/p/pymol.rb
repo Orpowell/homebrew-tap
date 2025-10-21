337035dcd693c125fdc102dc98afe24b0541458b
Oliver Powell <yok25vun@n125947.nbi.ac.uk>
2025-10-21 15:39:18 +0100

    reverted to 2.5.2 Pymol.rb

--- Formula/p/pymol.rb
+++ Formula/p/pymol.rb
@@ -1,89 +1,104 @@
 class Pymol < Formula
   include Language::Python::Virtualenv
-
   desc "Molecular visualization system"
   homepage "https://pymol.org/"
-  url "https://github.com/schrodinger/pymol-open-source/archive/refs/tags/v3.1.0.tar.gz"
-  sha256 "54306d65060bd58ed8b3dab1a8af521aeb4fd417871f15f463ff05ccb4e121fe"
+  url "https://github.com/schrodinger/pymol-open-source/archive/refs/tags/v2.5.0.tar.gz"
+  sha256 "aa828bf5719bd9a14510118a93182a6e0cadc03a574ba1e327e1e9780a0e80b3"
   license :cannot_represent
-  revision 1
   head "https://github.com/schrodinger/pymol-open-source.git", branch: "master"
 
   bottle do
     rebuild 2
-    sha256 cellar: :any,                 arm64_tahoe:   "05c7ebc310a6c2ec2e91da1a897470ef61750b97c18d31879c80e9562bb4c7a0"
-    sha256 cellar: :any,                 arm64_sequoia: "262b8a1c6702c62c9e21d3593e0575e5496165b9d93d67841d9d2faa6ac94f67"
-    sha256 cellar: :any,                 arm64_sonoma:  "77dfd4698b6e5b90f3e4a9117301c020e6b3b67b16e5c76e1886a0f17a1c5612"
-    sha256 cellar: :any,                 sonoma:        "4958fb877f750f6d443c407c265a73839dfdc98a600845ca6f90af766d26925d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c162a6c917186bb44563bcf497476a0a5c7e4be2c2847a9337ac282d4831c396"
+    sha256 cellar: :any,                 arm64_sonoma:   "1b3edf37eaeb5b3c38e5fdf9f6a72f4eafecb1f4ea0bb7a9208b0a4e41202a1f"
+    sha256 cellar: :any,                 arm64_ventura:  "33fb5ec69774436e99803273fe04c69ec04d4af245361b3ef916141e867c0b08"
+    sha256 cellar: :any,                 arm64_monterey: "e2c6c18ea8a6643a96692e427d31b33cf21003c30ec74fadc8aa019b20e881d0"
+    sha256 cellar: :any,                 sonoma:         "0d8fa8d46524c5c87123860a1fa0641a1ed0390ab6b2230301f055ad95e8a6a7"
+    sha256 cellar: :any,                 ventura:        "2172933239097915902897e01cbef8bf505aa80ec88cc9c1a8028c48de982aed"
+    sha256 cellar: :any,                 monterey:       "e9580cbfe839e0ed7cc37d13982ac6928684ff8baf426fc9b23bd2a1ec668290"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ecb4cb75a88e52dcdbced60b2bd41dddf14bb5c3671e23737b5b20e6ae4a1a2"
   end
 
   depends_on "cmake" => :build
   depends_on "glm" => :build
   depends_on "msgpack-cxx" => :build
   depends_on "sip" => :build
-
   depends_on "freetype"
   depends_on "glew"
   depends_on "libpng"
   depends_on "netcdf"
   depends_on "numpy"
-  depends_on "pyqt"
-  depends_on "python@3.10"
-
+  depends_on "pyqt@5"
+  depends_on "python-setuptools" # for pymol/plugins/installation.py
+  depends_on "python@3.12"
   uses_from_macos "libxml2"
 
   on_linux do
-    depends_on "patchelf" => :build
     depends_on "freeglut"
-    depends_on "mesa"
   end
 
   resource "mmtf-cpp" do
-    url "https://github.com/rcsb/mmtf-cpp/archive/refs/tags/v1.1.0.tar.gz"
-    sha256 "021173bdc1814b1d0541c4426277d39df2b629af53151999b137e015418f76c0"
+    url "https://github.com/rcsb/mmtf-cpp/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "881f69c4bb56605fa63fd5ca50842facc4947f686cbf678ad04930674d714f40"
   end
 
   resource "msgpack" do
-    url "https://files.pythonhosted.org/packages/cb/d0/7555686ae7ff5731205df1012ede15dd9d927f6227ea151e901c7406af4f/msgpack-1.1.0.tar.gz"
-    sha256 "dd432ccc2c72b914e4cb77afce64aab761c1137cc698be3984eee260bcb2896e"
+    url "https://files.pythonhosted.org/packages/61/3c/2206f39880d38ca7ad8ac1b28d2d5ca81632d163b2d68ef90e46409ca057/msgpack-1.0.3.tar.gz"
+    sha256 "51fdc7fb93615286428ee7758cecc2f374d5ff363bdd884c7ea622a7a327a81e"
   end
 
   resource "mmtf-python" do
-    url "https://files.pythonhosted.org/packages/d8/0f/f3c132dc9aac9a3f32a0eba7a80f07d14e7624e96f9245eeac5fe48f42cd/mmtf-python-1.1.3.tar.gz"
-    sha256 "12a02fe1b7131f0a2b8ce45b46f1e0cdd28b9818fe4499554c26884987ea0c32"
+    url "https://files.pythonhosted.org/packages/13/ea/c6a302ccdfdcc1ab200bd2b7561e574329055d2974b1fb7939a7aa374da3/mmtf-python-1.1.2.tar.gz"
+    sha256 "a5caa7fcd2c1eaa16638b5b1da2d3276cbd3ed3513f0c2322957912003b6a8df"
   end
 
-  resource "pmw" do
+  resource "Pmw" do
     url "https://github.com/schrodinger/pmw-patched/archive/8bedfc8747e7757c1048bc5e11899d1163717a43.tar.gz"
     sha256 "3a59e6d33857733d0a8ff0c968140b8728f8e27aaa51306160ae6ab13cea26d3"
   end
 
-  # Allow numpy 2+, remove on next release
-  patch do
-    url "https://github.com/schrodinger/pymol-open-source/commit/9ea504ea8d5400b195e72ae84e221ddfee54ee45.patch?full_index=1"
-    sha256 "9ea504ea8d5400b195e72ae84e221ddfee54ee45"
-  end
-
   def python3
-    which("python3.10")
+    which("python3.12")
   end
 
   def install
+    site_packages = Language::Python.site_packages(python3)
+    ENV.prepend_path "PYTHONPATH", Formula["numpy"].opt_prefix/site_packages
+
     resource("mmtf-cpp").stage do
       system "cmake", "-S", ".", "-B", "build", *std_cmake_args(install_prefix: buildpath/"mmtf")
       system "cmake", "--build", "build"
       system "cmake", "--install", "build"
     end
 
-    inreplace "setup.py" do |s|
-      s.gsub!(/ no_glut = True$/, " no_glut = False")
-      s.gsub!(/ use_msgpackc = "guess"$/, ' use_msgpackc = "c++11"')
+    # install other resources
+    resources.each do |r|
+      next if r.name == "mmtf-cpp"
+
+      r.stage do
+        system python3, *Language::Python.setup_install_args(libexec, python3)
+      end
     end
 
-    ENV["PREFIX_PATH"] = "#{buildpath}/mmtf:#{ENV["CMAKE_PREFIX_PATH"]}"
-    venv = virtualenv_install_with_resources without: "mmtf-cpp"
-    (prefix/Language::Python.site_packages(python3)/"homebrew-pymol.pth").write venv.site_packages
+    if OS.linux?
+      # Fixes "libxml/xmlwriter.h not found" on Linux
+      ENV.append "LDFLAGS", "-L#{Formula["libxml2"].opt_lib}"
+      ENV.append "CPPFLAGS", "-I#{Formula["libxml2"].opt_include}/libxml2"
+    end
+    # CPPFLAGS freetype2 required.
+    ENV.append "CPPFLAGS", "-I#{Formula["freetype"].opt_include}/freetype2"
+    # Point to vendored mmtf headers.
+    ENV.append "CPPFLAGS", "-I#{buildpath}/mmtf/include"
+
+    args = %W[
+      --install-scripts=#{libexec}/bin
+      --install-lib=#{libexec/site_packages}
+      --glut
+      --use-msgpackc=c++11
+    ]
+
+    system python3, "setup.py", "install", *args
+    (prefix/site_packages/"homebrew-pymol.pth").write libexec/site_packages
+    bin.install libexec/"bin/pymol"
   end
 
   def caveats
@@ -91,15 +106,14 @@ class Pymol < Formula
   end
 
   test do
-    (testpath/"test.py").write <<~PYTHON
+    (testpath/"test.py").write <<~EOS
       from pymol import cmd
       cmd.fragment('ala')
       cmd.zoom()
       cmd.png("test.png", 200, 200)
-    PYTHON
-
-    system bin/"pymol", "-cq", testpath/"test.py"
-    assert_path_exists testpath/"test.png", "Amino acid image should exist"
+    EOS
+    system "#{bin}/pymol", "-cq", testpath/"test.py"
+    assert_predicate testpath/"test.png", :exist?, "Amino acid image should exist"
     system python3, "-c", "import pymol"
   end
 end
